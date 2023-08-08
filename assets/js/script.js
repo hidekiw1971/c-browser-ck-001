8706cbdae5caa9f2fdc0c5eadd503c0e95c5c110
hideki watabe <hideki.w.1971@gmail.com>
2023-08-09 08:45:40 +0900

    2023/8/9 8:45

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
+"use strict";
 
-jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+jQuery(function ($) {
+  // この中であればWordpressでも「$」が使用可能になる
   // start
   //ドロワーメニュー
   $(".js-drawer").click(function () {
     $(".js-drawer").toggleClass("active");
   });
-
   // end
 });
--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
   // start
   //ドロワーメニュー
-  $("#MenuButton").click(function () {
+  $(".js-drawer").click(function () {
     $(".js-drawer").toggleClass("active");
   });
 
